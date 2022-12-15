c5b7214c372ec5ded517c77c66addb49eb2f9a14
Tommimon <tommimon@gmail.com>
2022-12-15 17:23:44 +0100

    Sor and gauss solvers

---
+++ gauss.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+void gauss_seidel(std::vector<std::vector<double>>& grid,
+                  const std::vector<std::vector<double>>& rhs,
+                  double relaxation_param)
+{
+    // Get the dimensions of the grid
+    int nx = grid.size();
+    int ny = grid[0].size();
+
+    // Perform Gauss-Seidel iteration on each point in the grid
+    for (int i = 1; i < nx-1; i++)
+    {
+        for (int j = 1; j < ny-1; j++)
+        {
+            // Update the value at this point using the Gauss-Seidel formula
+            grid[i][j] = (1 - relaxation_param) * grid[i][j] +
+                         relaxation_param * (grid[i-1][j] + grid[i+1][j] + grid[i][j-1] + grid[i][j+1] - rhs[i][j]) / 4.0;
+        }
+    }
+}
---
+++ jacobi.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+
+// Function for performing Jacobi iteration on a grid of values
+void jacobi(std::vector<std::vector<double>>& grid,
+            const std::vector<std::vector<double>>& rhs,
+            double relaxation_param)
+{
+    // Get the dimensions of the grid
+    int nx = grid.size();
+    int ny = grid[0].size();
+
+    // Temporary grid for storing updated values
+    std::vector<std::vector<double>> temp(nx, std::vector<double>(ny));
+
+    // Perform Jacobi iteration on each point in the grid
+    for (int i = 1; i < nx-1; i++)
+    {
+        for (int j = 1; j < ny-1; j++)
+        {
+            // Update the value at this point using the Jacobi formula
+            temp[i][j] = (1 - relaxation_param) * grid[i][j] +
+                         relaxation_param * (grid[i-1][j] + grid[i+1][j] + grid[i][j-1] + grid[i][j+1] - rhs[i][j]) / 4.0;
+        }
+    }
+
+    // Update the grid with the new values
+    grid = temp;
+}
