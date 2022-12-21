8fae1576adfcbcc71bb1bcbeca46f263cefa9af5
Tommimon <tommimon@gmail.com>
2022-12-21 01:27:43 +0100

    Updated MPI build command

--- build_mpi.sh
+++ build_mpi.sh
@@ -1,1 +1,1 @@
-flatpak-spawn --host mpiCC -D USE_MPI main.cpp jacobi.cpp gauss.cpp sor.cpp multigrid.cpp -o cmake-build-debug/main.out
+flatpak-spawn --host mpiCC -D USE_MPI main.cpp jacobi_mpi.cpp gauss_mpi.cpp sor_mpi.cpp multigrid.cpp -o cmake-build-debug/main.out
