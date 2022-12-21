edfa29dea0ae46ec96bc48e498d392a059c29339
Tommimon <tommimon@gmail.com>
2022-12-21 17:37:23 +0100

    Completed exercise 2.2

--- 2-1.sh
+++ 2-1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 g++  main.cpp jacobi.cpp gauss.cpp sor.cpp multigrid.cpp -o main.out
-./main.out 40 2 j > serial.log
+./main.out 40 2 10 j > serial.log
 g++ -fopenmp main.cpp jacobi_omp.cpp gauss_omp.cpp sor_omp.cpp multigrid.cpp -o main.out
-./main.out 40 2 j > parallel.log
+./main.out 40 2 10 j > parallel.log
 diff parallel.log serial.log
