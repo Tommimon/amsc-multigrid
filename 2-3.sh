1593aadffb49a145310d4ea7f72ead6da32f6180
Tommimon <tommimon@gmail.com>
2022-12-22 02:25:56 +0100

    Changed parameters of simulation

--- 2-3.sh
+++ 2-3.sh
@@ -8,12 +8,12 @@ time ./main.out 260 3 100 10 j
 
 echo gauss
 g++  main.cpp jacobi.cpp gauss.cpp sor.cpp multigrid.cpp -o main.out
-time ./main.out 260 3 100 10 g
+time ./main.out 2600 3 1 10 g
 g++ -fopenmp main.cpp jacobi_omp.cpp gauss_omp.cpp sor_omp.cpp multigrid.cpp -o main.out
-time ./main.out 260 3 100 10 g
+time ./main.out 2600 3 1 10 g
 
 echo sor
 g++  main.cpp jacobi.cpp gauss.cpp sor.cpp multigrid.cpp -o main.out
-time ./main.out 260 3 100 10 s
+time ./main.out 2600 3 1 10 s
 g++ -fopenmp main.cpp jacobi_omp.cpp gauss_omp.cpp sor_omp.cpp multigrid.cpp -o main.out
-time ./main.out 260 3 100 10 s
+time ./main.out 2600 3 1 10 s
